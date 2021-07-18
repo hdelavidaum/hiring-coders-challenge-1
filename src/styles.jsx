b044689aef1de3f1bb7721197db8ec5498c919d4
Henrique Delavi Daum <henrique.delavi.daum@gmail.com>
2021-07-18 12:46:11 -0300

    feat: add css reset and styles to App

---
+++ src/styles.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const InputsWrapper = styled.div`
+    width: 50%;
+    height: 100%;
+
+    display: flex;
+    flex-flow: column nowrap;
+    place-content: center;
+    align-items: center;
+`
+
+export const ProductsWrapper = styled.div`
+    width: 50%;
+    height: 100%;
+
+    display: flex;
+    flex-flow: column nowrap;
+    place-content: center;
+    align-items: center;
+`
