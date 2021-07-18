9c18a5250f54b1166e7354c0eb22ea2732d1db0c
Henrique Delavi Daum <henrique.delavi.daum@gmail.com>
2021-07-17 21:55:36 -0300

    chore:las modifications before run yarn eject

---
+++ src/components/first-component/index.tsx
@@ -0,0 +1,3 @@
+const ComponenteDeTeste = (): JSX.Element => <h1>É isso rapá</h1>
+
+export default ComponenteDeTeste
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import ComponenteDeTeste from "../components/first-component"
+
+const PaginaTeste = ():JSX.Element => <div><ComponenteDeTeste/></div>
+
+export default PaginaTeste
